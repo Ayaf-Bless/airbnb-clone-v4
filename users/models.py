e6ef16f237476496beb4d5a7b0f1fa901062af72
ayaf-bless <blessambel1@gmail.com>
2021-12-23 23:06:03 +0300

    complete user-module part-1

--- users/models.py
+++ users/models.py
@@ -7,13 +7,19 @@ class User(AbstractUser):
     GENDER_MALE = "male"
     GENDER_FEMALE = "female"
     GENDER_OTHER = "other"
-    GENDER_CHOICE = (
-        (GENDER_MALE, "Male"),
-        (GENDER_FEMALE,"Female"),
-        (GENDER_OTHER, "Other")
-    )
+    GENDER_CHOICES = ((GENDER_MALE, "Male"), (GENDER_FEMALE, "Female"), (GENDER_OTHER, "Other"))
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_FRENCH = "kr"
+    LANGUAGE_CHOICES = ((LANGUAGE_FRENCH, "French"), (LANGUAGE_ENGLISH, "English"))
 
-    bio = models.TextField(default="", blank=True)
-    avatar = models.ImageField(null=True, blank=True)
-    gender = models.CharField(choices=GENDER_CHOICE, max_length=10, null=True, blank=True)
+    CURRENCY_USD = "usd"
+    CURRENCY_EU = "eu"
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_EU, "EU"))
 
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
+    birth_date = models.DateField(blank=True,null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=5, blank=True)
+    super_host = models.BooleanField(default=False)
