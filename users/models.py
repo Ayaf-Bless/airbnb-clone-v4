9d772fdd56f70c130ef10710fd462979f5da2f30
ayaf-bless <blessambel1@gmail.com>
2021-12-23 11:43:07 +0300

    add field

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    """ User Model """
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+    GENDER_CHOICE = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE,"Female"),
+        (GENDER_OTHER, "Other")
+    )
+
+    bio = models.TextField(default="", blank=True)
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(choices=GENDER_CHOICE, max_length=10, null=True, blank=True)
+
