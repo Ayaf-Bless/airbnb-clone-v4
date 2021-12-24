65d3b2c6c898af527d82c4b2d594dbe09e817e36
ayaf-bless <blessambel1@gmail.com>
2021-12-24 16:48:01 +0300

    add model and migration for romm part-1

--- core/models.py
+++ core/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Core(models.Model):
     """Time stamped model"""
-    created_at = models.DateField()
-    updated_at = models.DateField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
 
     class Meta:
         abstract = True
--- rooms/models.py
+++ rooms/models.py
@@ -1,6 +1,23 @@
 from django.db import models
+from django_countries.fields import CountryField
+# own imports
+from core import models as core_model
+from users import models as users_model
 
 
-class Room(models.Model):
+class Room(core_model.Core):
     """ Rooms model """
-    pass
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    country = CountryField()
+    city = models.CharField(max_length=80)
+    price = models.IntegerField()
+    address = models.CharField(max_length=140)
+    beds = models.IntegerField()
+    bedrooms = models.IntegerField()
+    baths = models.IntegerField()
+    guests = models.IntegerField()
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(to=users_model.User, on_delete=models.CASCADE)
