7761a4608b9ac98be8bcfe5bf9b8334714f8de27
ayaf-bless <blessambel1@gmail.com>
2021-12-26 02:04:44 +0300

    add chat's model to the mix

--- chats/models.py
+++ chats/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class Chat(core_models.Core):
+    participants = models.ManyToManyField("users.User", blank=True)
+
+    def __str__(self):
+        return str(self.created_at)
+
+
+class Message(core_models.Core):
+    message = models.TextField()
+    user = models.ForeignKey(to="users.User", on_delete=models.CASCADE)
+    chat = models.ForeignKey(to=Chat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user} says... {self.message}"
