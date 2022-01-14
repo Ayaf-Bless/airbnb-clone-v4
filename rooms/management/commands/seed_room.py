decf76b52ba9fa9e35f848145f46a76a560fed30
ayaf-bless <blessambel1@gmail.com>
2022-01-14 18:05:33 +0300

    seeding bedroom command part-2

--- rooms/management/commands/seed_room.py
+++ rooms/management/commands/seed_room.py
@@ -1,6 +1,6 @@
 from abc import ABC
 import random
-
+from django.contrib.admin.utils import flatten
 from django_seed import Seed
 from django.core.management.base import BaseCommand
 from users import models as user_models
@@ -28,5 +28,13 @@ class Command(BaseCommand, ABC):
             "bedrooms": lambda x: random.randint(1, 5),
             "name": lambda x: seeder.faker.address()
         })
-        seeder.execute()
-        self.stdout.write(self.style.SUCCESS(f"{number} rooms were created!"))
+        created_photos = seeder.execute()
+        created_clean = flatten(list(created_photos.values()))
+        for pk in created_clean:
+            room = room_models.Room.objects.get(pk=pk)
+            for i in range(3, random.randint(10, 17)):
+                room_models.Photo.objects.create(caption=seeder.faker.sentence(),
+                                                 file=f"/room_photo/{random.randint(1, 31)}.webp",
+                                                 room=room,
+                                                 )
+            self.stdout.write(self.style.SUCCESS(f"{number} rooms were created!"))
