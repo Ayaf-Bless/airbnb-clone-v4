8af086d34c10f97ac3c71d84a09c0d75623095d1
ayaf-bless <blessambel1@gmail.com>
2022-01-15 21:23:09 +0300

    setting pagination manually

--- rooms/views.py
+++ rooms/views.py
@@ -5,7 +5,11 @@ from . import models as room_models
 
 
 # Create your views here.
-def get_rooms(request: HttpRequest): 
-    all_rooms = room_models.Room.objects.all()
+def get_rooms(request: HttpRequest):
+    page = int(request.GET.get("page", 1))
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = room_models.Room.objects.all()[offset:limit]
     number = 44
     return render(request, "rooms/home.html", context={"rooms": all_rooms})
