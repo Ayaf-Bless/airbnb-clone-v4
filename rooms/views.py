9b3697b195614997f7bb6ebd6b26a7b026cefe01
ayaf-bless <blessambel1@gmail.com>
2022-01-15 12:31:27 +0300

    dealing with request and response part-1

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
 
 # Create your views here.
-def all_rooms(request):
-    pass
+def all_rooms(request: HttpRequest):
+    return render(request,"all_rooms")
