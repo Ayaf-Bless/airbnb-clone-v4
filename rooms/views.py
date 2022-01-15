d1d43ce9f27db619ec5598459727a883442a7c81
ayaf-bless <blessambel1@gmail.com>
2022-01-15 17:47:50 +0300

    introduicing templating, parials, blocks and include

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+from . import models as room_models
 
 
 # Create your views here.
-def all_rooms(request: HttpRequest):
+def get_rooms(request: HttpRequest): 
+    all_rooms = room_models.Room.objects.all()
     number = 44
-    return render(request, "all_rooms.html", context={"number": number, "ok": True})
+    return render(request, "rooms/home.html", context={"rooms": all_rooms})
