15f064299103732cec6e9d4a22345ea29b313eca
ayaf-bless <blessambel1@gmail.com>
2022-01-16 17:43:05 +0300

    pagination using class based view part-1

--- rooms/views.py
+++ rooms/views.py
@@ -1,26 +1,32 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+# from django.http import HttpRequest, HttpResponse
+# from django.shortcuts import render, redirect
+# from django.core.paginator import Paginator, EmptyPage
 
+from django.views.generic import ListView
 from . import models as room_models
 
 
-# Create your views here.
-def get_rooms(request: HttpRequest):
-    page = int(request.GET.get("page") or 1)
-    all_rooms = room_models.Room.objects.all()
-    paginator = Paginator(all_rooms, 10, orphans=5)
-    try:
-        rooms = paginator.page(page)
-        return render(request, "rooms/home.html",
-                      context={"rooms": rooms})
-    except EmptyPage:
-        return redirect("/")
-
-    # page_size = 10
-    # limit = page_size * page
-    # page_count = ceil(room_models.Room.objects.count() / page_size)
-    # offset = limit - page_size
-    # all_rooms = room_models.Room.objects.all()[offset:limit]
-    # number = 44
-
+class HomeView(ListView):
+    model = room_models.Room
+    paginate_by = 10
+    ordering = "created_at"
+    paginate_orphans = 5
+#
+# # Create your views here.
+# def get_rooms(request: HttpRequest):
+#     page = int(request.GET.get("page") or 1)
+#     all_rooms = room_models.Room.objects.all()
+#     paginator = Paginator(all_rooms, 10, orphans=5)
+#     try:
+#         rooms = paginator.page(page)
+#         return render(request, "rooms/home.html",
+#                       context={"rooms": rooms})
+#     except EmptyPage:
+#         return redirect("/")
+#
+#     # page_size = 10
+#     # limit = page_size * page
+#     # page_count = ceil(room_models.Room.objects.count() / page_size)
+#     # offset = limit - page_size
+#     # all_rooms = room_models.Room.objects.all()[offset:limit]
+#     # number = 44
