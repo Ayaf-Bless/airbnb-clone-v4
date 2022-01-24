ea0d5e4d9df5f45adca91a6541fc547a9e65d798
ayaf-bless <blessambel1@gmail.com>
2022-01-24 14:18:09 +0300

    fixing up the csrf attack with django

--- users/views.py
+++ users/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.views import View
+from . import forms
 
 
 # Create your views here.
 class LoginView(View):
     def get(self, request):
-        return render(request, template_name="users/login.html")
+        form = forms.LoginForm()
+        return render(request, template_name="users/login.html", context={"form": form})
 
     def post(self, request):
-        pass
+        form = forms.LoginForm(request.POST)
+        print(form)
