c9db5477b7278da0a6d19356668f16598fdbeab9
ayaf-bless <blessambel1@gmail.com>
2022-01-24 06:00:16 +0300

    utilizing form api to generate the form part-4

--- rooms/views.py
+++ rooms/views.py
@@ -71,10 +71,16 @@ class SearchView(View):
 
                 for facility in facilities:
                     filter_args["facilities"] = facility
-                rooms = room_models.Room.objects.filter(**filter_args)
+                qs = room_models.Room.objects.filter(**filter_args).order_by("created_at")
+                paginator = Paginator(qs, 10, orphans=5)
+                page = request.GET.get("page", 1)
+                rooms = paginator.get_page(page)
+                return render(request, "rooms/search.html",
+                              {"form": form, "rooms": rooms
+                               })
         else:
             form = forms.SearchForm()
 
-        return render(request, "rooms/search.html",
-                      {"form": form, "rooms": rooms
-                       })
+            return render(request, "rooms/search.html",
+                          {"form": form
+                           })
