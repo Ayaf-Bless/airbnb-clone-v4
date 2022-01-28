b986aa42f18b716b78e985ece815f9a7a9a654d8
ayaf-bless <blessambel1@gmail.com>
2022-01-28 16:08:49 +0300

    setting up login with github part-1

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("login", views.LoginView.as_view(), name="login"),
     path("logout", views.log_out, name="logout"),
     path("signup", views.SignUpView.as_view(), name="signup"),
-    path("verify/<str:token>", views.complete_email_verification, name="complete-verification")
+    path("verify/<str:token>", views.complete_email_verification, name="complete-verification"),
+    path("login/github", views.github_login, name="github_login"),
+    path("login/github/callback", views.github_callback, name="github_callback"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, render, reverse
 from django.urls import reverse_lazy
@@ -55,3 +57,14 @@ def complete_email_verification(request, token):
     except models.User.DoesNotExist:
         pass
     return redirect(reverse("core:home"))
+
+
+def github_login(request):
+    client_id = os.environ.get('GITHUB_CLIENT_ID')
+    redirect_url = "http://127.0.0.1:8000/users/login/github/callback"
+    return redirect(
+        f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_url}&scope=read:user")
+
+
+def github_callback(request):
+    pass
